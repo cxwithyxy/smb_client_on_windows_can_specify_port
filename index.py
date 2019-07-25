0470abebd56cf9348c0ea4720968ad6a9e89a9a8
chenxuan <523764720@qq.com>
2019-07-25 10:20:41 +0800

    增加版本号的显示

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from file_io_emulation.Server import Server as FS_server
 
 FS_server().start()
+print("\n\nsmb_client v0.201907251016")
 while(True):
     a = input("\n\nServer start ! enter q for exit \n======>\n")
     if(a == "stop"):
